1192f924f8e1d2e014b9744d51274bf66ea56c7d
Andrey Smirnov <and.inbx@gmail.com>
2018-09-12 00:15:06 +0300

    New -RTE argument, 'show_errors' setting

--- nose2-rt/rt.py
+++ nose2-rt/rt.py
@@ -19,6 +19,8 @@ class Rt(Plugin):
 
         self.endpoint = self.config.as_str(
             'endpoint', '')
+        self.show_errors = self.config.as_bool(
+            'show_errors', '')
 
         self.uuid = str(uuid.uuid4())
         self.success = 0
@@ -29,17 +31,30 @@ class Rt(Plugin):
         self.start = None
         self.stop = None
         self.test_outcome = None
+        self.attrs = []
+        self.addArgument(self.attrs, "RTE" "rtenv")
 
     def post(self, payload):
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        requests.post(self.endpoint, data=json.dumps(payload), headers=headers)
+        try:
+            requests.post(self.endpoint, data=json.dumps(payload), headers=headers)
+        except requests.exceptions.ConnectionError as error:
+            if self.show_errors:
+                print(error)
+            else:
+                pass
 
     def startTestRun(self, event):
         tests = self.getTests(event)
+        if len(self.attrs) > 0:
+            env = self.attrs[0]
+        else:
+            env = ""
         self.post({
             'type': "startTestRun",
             'job_id': self.uuid,
             'tests': tests,
+            'env': env,
             'startTime': str(event.startTime)
         })
 
